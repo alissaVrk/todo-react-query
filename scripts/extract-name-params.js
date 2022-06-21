49844ff0db115f54a8de0c1222ff8184c756b473
alissa verkhovsky <alissa.vrk@gmail.com>
2022-06-21 17:41:25 +0300

    Release/0.10/ewewe (#4)
    * next release 0.9.0
    
    * create hotfix
    
    * change
    
    * fix
    
    * debug
    
    * try
    
    * use script from main
    
    * branch name step
    
    * add token
    
    * bump to 0.10
    
    * publish
    
    * bump version 0.10.1
    
    * next release 0.11.0
    
    * a commit
    
    * fix release match
    
    * commit 1
    
    * fix pattern in release
    
    * on push release
    
    * try using env var
    
    * yarn before running code
    
    * bump version 0.10.2
    
    * bump version 0.10.3
    
    * run on pr
    
    * commit 2 (#1)
    
    * bump version 0.10.4
    
    * fix
    
    * commit 3 (#2)
    
    * bump version 0.10.5
    
    * gix ng branch name
    
    * commit 4 (#3)
    
    * bump version 0.10.6
    
    Co-authored-by: GitHub Actions Bot <>

---
+++ scripts/extract-name-params.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+
+const branch  = process.env.branch;
+
+console.log('BBBB', branch);
+
+const parts = branch.split('/');
+const desc = parts[parts.length - 1]; 
+const version = parts[parts.length - 2];
+
+core.setOutput('version', version);
+core.setOutput('desc', desc);
--- src/core/queryClient.ts
+++ src/core/queryClient.ts
@@ -64,3 +64,5 @@ export function registerQueryDependency<T>(from: UseQueryOptions<T>, to: QueryKe
     };
   }
 }
+
+//some shit asdsd asd
