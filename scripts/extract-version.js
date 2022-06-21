3afb5eddb00101bb03d692c0ebbf8ef13eb1e411
Alissa <alissa.vrk@gmail.com>
2022-06-21 10:59:06 +0300

    create hotfix

--- scripts/extract-version.js
+++ scripts/extract-version.js
@@ -4,3 +4,4 @@ const pkg = require('../package.json');
 const fullVersion = pkg.version;
 const [major, minor] = fullVersion.split('.');
 core.setOutput('version', `${major}.${minor}`);
+core.setOutput('full_version', fullVersion);
