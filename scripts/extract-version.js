97d477a5722546d2ebb30d873a190e5f9c9fb51c
Alissa <alissa.vrk@gmail.com>
2022-06-21 15:05:30 +0300

    script has to be in production

--- scripts/extract-version.js
+++ scripts/extract-version.js
@@ -3,4 +3,6 @@ const pkg = require('../package.json');
 
 const fullVersion = pkg.version;
 const [major, minor] = fullVersion.split('.');
+console.log('JJJJ', pkg.version, fullVersion, major, minor);
 core.setOutput('version', `${major}.${minor}`);
+core.setOutput('full_version', pkg.version);
