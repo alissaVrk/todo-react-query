67192d60e83af271981d404c3dd5aad68f22aa92
Alissa <alissa.vrk@gmail.com>
2022-06-22 11:09:04 +0300

    create merge branch

--- scripts/extract-name-params.js
+++ scripts/extract-name-params.js
@@ -5,8 +5,12 @@ const branch  = process.env.branch;
 console.log('BBBB', branch);
 
 const parts = branch.split('/');
+const isHotfix = parts[2] === 'hotfix';
+
 const desc = parts[parts.length - 1]; 
-const version = parts[parts.length - 2];
+const version = isHotfix ? parts[parts.length - 3] : parts[parts.length - 2];
+const ngVersion = isHotfix ? parts[parts.length - 2] : '';
 
 core.setOutput('version', version);
 core.setOutput('desc', desc);
+core.setOutput('ngVersion', ngVersion);
