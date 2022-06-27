9d25b300c200b1a07f68cb5cc6ab9242cb4610ca
Alissa <alissa.vrk@gmail.com>
2022-06-27 14:40:30 +0300

    debug

--- scripts/extract-name-params.js
+++ scripts/extract-name-params.js
@@ -1,4 +1,5 @@
 module.exports = (branch) => {
+  console.log(branch);
   const parts = branch.split("/");
   const isHotfix = parts[2] === "hotfix";
 
@@ -6,9 +7,11 @@ module.exports = (branch) => {
   const version = isHotfix ? parts[parts.length - 3] : parts[parts.length - 2];
   const ngVersion = isHotfix ? parts[parts.length - 2] : "";
 
-  return {
+  const ret = {
     version,
     desc,
     ngVersion,
   };
+  console.log('RRR', ret);
+  return ret;
 };
