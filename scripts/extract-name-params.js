bd74409e81b5abc3b0567a3a901f3d2490049925
Alissa <alissa.vrk@gmail.com>
2022-06-27 10:10:32 +0300

    refactor extract params

--- scripts/extract-name-params.js
+++ scripts/extract-name-params.js
@@ -1,16 +1,14 @@
-const core = require('@actions/core');
+module.exports = (branch) => {
+  const parts = branch.split("/");
+  const isHotfix = parts[2] === "hotfix";
 
-const branch  = process.env.branch;
+  const desc = parts[parts.length - 1];
+  const version = isHotfix ? parts[parts.length - 3] : parts[parts.length - 2];
+  const ngVersion = isHotfix ? parts[parts.length - 2] : "";
 
-console.log('BBBB', branch);
-
-const parts = branch.split('/');
-const isHotfix = parts[2] === 'hotfix';
-
-const desc = parts[parts.length - 1]; 
-const version = isHotfix ? parts[parts.length - 3] : parts[parts.length - 2];
-const ngVersion = isHotfix ? parts[parts.length - 2] : '';
-
-core.setOutput('version', version);
-core.setOutput('desc', desc);
-core.setOutput('ngVersion', ngVersion);
+  return {
+    version,
+    desc,
+    ngVersion,
+  };
+};
