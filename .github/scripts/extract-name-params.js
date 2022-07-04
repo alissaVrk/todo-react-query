7bf412f720bfc33065528059c945826dcc8b3da9
Alissa <alissa.vrk@gmail.com>
2022-07-04 17:16:33 +0300

    delete hotfix desc

--- .github/scripts/extract-name-params.js
+++ .github/scripts/extract-name-params.js
@@ -4,11 +4,14 @@ module.exports = (core, branch) => {
   const parts = branch.split("/");
   const isHotfix = parts[0] === "hotfix";
 
-  const desc = parts[parts.length - 1];
-  const version = isHotfix ? parts[parts.length - 3] : parts[parts.length - 2];
-  const ngVersion = isHotfix ? parts[parts.length - 2] : "";
-
+  if (!isHotfix) {
+    const desc = parts[parts.length - 1];
+    core.setOutput("desc", desc);
+  } else {
+    const ngVersion = parts[parts.length - 1];
+    core.setOutput("ngVersion", ngVersion);
+  }
+  
+  const version = parts[parts.length - 2];
   core.setOutput("version", version);
-  core.setOutput("desc", desc);
-  core.setOutput("ngVersion", ngVersion);
 };
